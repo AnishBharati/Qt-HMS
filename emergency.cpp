26d2addcc0e623aead1733e134e0013bf0709bdf
RaghavSitaula <113698501+RaghavSitaula@users.noreply.github.com>
2022-11-22 20:53:45 +0545

    viber

---
+++ emergency.cpp
@@ -0,0 +1,86 @@
+#include "emergency.h"
+#include "ui_emergency.h"
+#include"login.h"
+#include<QDebug>
+#include<QStringList>
+#include<QSortFilterProxyModel>
+#include<QUrl>>
+#include<QDesktopServices>>
+#include<QFileDialog>
+emergency::emergency(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::emergency)
+{
+    ui->setupUi(this);
+}
+
+emergency::~emergency()
+{
+    delete ui;
+}
+
+void emergency::on_pushButton_load_table_clicked()
+{
+    Login conn;
+    QSqlQueryModel * modal100=new QSqlQueryModel();
+
+    conn.connOpen();
+    QSqlQuery* qry=new QSqlQuery(conn.mydb);
+    qry->prepare("select * from doctorsinfo");
+    qry->exec();
+    modal100->setQuery(*qry);
+
+
+
+    proxyPersonas1=new QSortFilterProxyModel(this);
+    proxyPersonas1->setSourceModel(modal100);
+    proxyPersonas1->setFilterCaseSensitivity(Qt::CaseInsensitive);
+    proxyPersonas1->setFilterKeyColumn(-1);
+    //proxyPersonas1->setFilterFixedString("ko");
+
+    ui->cbColumnas->addItems(QStringList()<<"ID"<<"Name"<<"Phone"<<"Email"<<"Specialist");
+
+
+    ui->tableView->setModel(proxyPersonas1);
+
+
+    conn.connClose();
+    qDebug() <<(proxyPersonas1->rowCount());
+}
+
+
+void emergency::on_lineEdit_textChanged(const QString &arg1)
+{
+   proxyPersonas1->setFilterFixedString(arg1);
+}
+
+
+void emergency::on_cbColumnas_currentIndexChanged(int index)
+{
+    proxyPersonas1->setFilterKeyColumn(index);
+}
+
+
+void emergency::on_pushButton_back_clicked()
+{
+    this->hide();
+    patientsinfo Patientsinfo;
+    Patientsinfo.setModal(true);
+    Patientsinfo.exec();
+}
+
+
+void emergency::on_pushButton_clicked()
+{
+QString filename=QFileDialog::getOpenFileName(
+            this,
+            tr("Open file"),
+            "C://",
+                "All files (*.*);;Text File (*.txt);;Music File(*.mp3)"
+
+
+            );
+    QDesktopServices::openUrl(QUrl("file:///"+filename,QUrl::TolerantMode));
+    //QMessageBox::Information()
+
+}
---
+++ emergency.h
@@ -0,0 +1,34 @@
+#ifndef EMERGENCY_H
+#define EMERGENCY_H
+
+#include <QDialog>
+#include<QSortFilterProxyModel>
+namespace Ui {
+class emergency;
+}
+
+class emergency : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit emergency(QWidget *parent = nullptr);
+    ~emergency();
+    QSortFilterProxyModel *proxyPersonas1;
+
+private slots:
+    void on_pushButton_load_table_clicked();
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+    void on_cbColumnas_currentIndexChanged(int index);
+
+    void on_pushButton_back_clicked();
+
+    void on_pushButton_clicked();
+
+private:
+    Ui::emergency *ui;
+};
+
+#endif // EMERGENCY_H
